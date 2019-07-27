f768d15d4b978550e4907ffa2e870bbe36a2b611
HWenTing <982740445@qq.com>
2019-07-28 00:05:13 +0800

    树

--- T_401_500/T404.java
+++ T_401_500/T404.java
@@ -3,14 +3,19 @@ package T_401_500;
 public class T404 {
 
 //	左叶子之和
+	
 	private int sum=0;
     public int sumOfLeftLeaves(TreeNode root) {
     	if(root==null)
     		return 0;
-    	hasleft(root);
-        return sum;
+    	if (isLeaf(root.left)) 
+    		return root.left.val + sumOfLeftLeaves(root.right); 
+    	return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
     }
-    private void hasleft(TreeNode root){
-    	
+    
+    private boolean isLeaf(TreeNode node){ //判断是不是叶节点
+    	if (node == null) 
+    		return false; 
+    	return node.left == null && node.right == null; 
     }
 }
