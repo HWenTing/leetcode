d9541c0608b4d0adeccf368b1aaf4df21be63d87
HWenTing <982740445@qq.com>
2019-08-17 22:35:30 +0800

    字符串

--- T_1001/T1156.java
+++ T_1001/T1156.java
@@ -11,7 +11,7 @@ public class T1156 {
 	
 	
 //	就按笨办法按部就班试试
-	
+//	。。。速度击败10% 空间击败100%。。
 	List<int[]>[] cnt;
     public int maxRepOpt1(String text) {
     	cnt = new List[26];//存放每个字母出现的区间
@@ -24,7 +24,7 @@ public class T1156 {
         for(int i=1;i<text.length();i++){//统计每个字母出现的区间
         	char c = text.charAt(i);
         	if(c!=before){//和前一个不同
-        		cnt[c-'a'].add(new int[]{start,i,i-start});
+        		cnt[before-'a'].add(new int[]{start,i,i-start});
         		start = i;
         		before = c;//更新前邻字母
         	}
@@ -41,22 +41,22 @@ public class T1156 {
     
     private int getmax(int i){//返回某个字母的最长长度
     	int len = cnt[i].size();
+
     	if(len==0){//表示该字母没出现过
     		return 0;
     	}else if(len==1){//表示只出现在一段
     		return cnt[i].get(0)[1]-cnt[i].get(0)[0];
     	}else if(len==2){//表示只出现在两段
     		if(cnt[i].get(1)[0]-cnt[i].get(0)[1]==1){//两段只隔1位
-    			return cnt[i].get(1)[1]-cnt[i].get(0)[0];
+    			return cnt[i].get(1)[2]+cnt[i].get(0)[2];
     		}else{
     			return Math.max(cnt[i].get(0)[2],cnt[i].get(1)[2])+1;
     		}
     	}else{//出现多段，
-    		
     		int max = 0;
     		for(int pos=0;pos<len;pos++){
     			if(pos<len-1 && cnt[i].get(pos+1)[0]-cnt[i].get(pos)[1]==1){//两段隔一个
-    				max = Math.max(max, cnt[i].get(pos+1)[2]+cnt[i].get(pos)[2]);
+    				max = Math.max(max, cnt[i].get(pos+1)[2]+cnt[i].get(pos)[2]+1);
     			}else
     				max = Math.max(cnt[i].get(pos)[2]+1,max);
     		}
