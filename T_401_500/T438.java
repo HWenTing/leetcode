44b78905ac64e0b9359c4fcfdafc27c33e356eb3
HWenTing <982740445@qq.com>
2019-08-19 00:28:22 +0800

    滑动窗口 哈希表

--- T_401_500/T438.java
+++ T_401_500/T438.java
@@ -1,5 +1,6 @@
 package T_401_500;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -46,37 +47,67 @@ public class T438 {
 //    }
 	
 //	类似滑动窗口的思想，进来一个cnt++,出去一个cnt--,26位数组全为0说明ok
-//	击败58%
-    public List<Integer> findAnagrams(String s, String p) {
-    	int lens = s.length();
-    	int lenp = p.length();
-    	List<Integer> ans = new LinkedList<>();
-    	if(lens<lenp) return ans;
-    	
-    	int[] cnt = new int[26];
-    	for(char c:p.toCharArray()){//初始化
-    		cnt[c-'a']--;
-    	}
-    	int pos;
-    	for(pos=0;pos<lenp;pos++){//初始化
-    		cnt[s.charAt(pos)-'a']++;
-    	}
-    	for(;pos<lens;pos++){//从lenp位开始，启动滑动窗口思想
-    		if(check(cnt))
-    			ans.add(pos-lenp);
-    		
-    		cnt[s.charAt(pos)-'a']++;
-    		cnt[s.charAt(pos-lenp)-'a']--;
-    	}
-    	if(check(cnt)) ans.add(pos-lenp);
-    	return ans;
-    }
-    
-	private boolean check(int[] cnt){
-		for(int i:cnt)
-			if(i!=0)
-				return false;
-		return true;
-	}
+//	击败61%
+//    public List<Integer> findAnagrams(String s, String p) {
+//    	int lens = s.length();
+//    	int lenp = p.length();
+//    	List<Integer> ans = new LinkedList<>();
+//    	if(lens<lenp) return ans;
+//    	
+//    	int[] cnt = new int[26];
+//    	int pos;
+//    	for(pos=0;pos<lenp;pos++){//初始化
+//    		cnt[p.charAt(pos)-'a']--;
+//    		cnt[s.charAt(pos)-'a']++;
+//    	}
+//    	for(;pos<lens;pos++){//从lenp位开始，启动滑动窗口思想
+//    		if(check(cnt))
+//    			ans.add(pos-lenp);
+//    		
+//    		cnt[s.charAt(pos)-'a']++;
+//    		cnt[s.charAt(pos-lenp)-'a']--;
+//    	}
+//    	if(check(cnt)) ans.add(pos-lenp);
+//    	return ans;
+//    }
+//    
+//	private boolean check(int[] cnt){
+//		for(int i:cnt)
+//			if(i!=0)
+//				return false;
+//		return true;
+//	}
+	
+	
+//	不使用数组改为hashmap
+//	啊。。。。竟然又变慢了。。击败35%。。
+//    public List<Integer> findAnagrams(String s, String p) {
+//    	int lens = s.length();
+//    	int lenp = p.length();
+//    	List<Integer> ans = new LinkedList<>();
+//    	if(lens<lenp) return ans;
+//    	HashMap<Character,Integer> cnt = new HashMap<>();
+//    	
+//    	int pos;
+//    	for(pos=0;pos<lenp;pos++){//初始化
+//    		onep(cnt,p.charAt(pos),-1);
+//    		onep(cnt,s.charAt(pos),1);
+//    	}
+//    	for(;pos<lens;pos++){//从lenp位开始，启动滑动窗口思想
+//    		if(cnt.isEmpty())//为空表示满足
+//    			ans.add(pos-lenp);
+//    		onep(cnt,s.charAt(pos),1);
+//    		onep(cnt,s.charAt(pos-lenp),-1);
+//    	}
+//    	if(cnt.isEmpty()) ans.add(pos-lenp);
+//    	return ans;
+//    }
+//    
+//	private void onep(HashMap<Character,Integer> hashmap,char c,int symbol){
+//		hashmap.put(c, hashmap.getOrDefault(c, 0)+symbol);
+//		if(hashmap.get(c)==0)
+//			hashmap.remove(c);
+//	}
+	
 	
 }
