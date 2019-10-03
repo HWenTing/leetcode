ea1dab0596d344816d0e2c5977bbbf4c16920c00
HWenTing <982740445@qq.com>
2019-10-03 23:26:54 +0800

    简单题

---
+++ T_501_600/T509.java
@@ -0,0 +1,19 @@
+package T_501_600;
+
+public class T509 {
+
+//	斐波那契数列
+    public int fib(int N) {
+    	if(N<2) return N;
+    	
+        int a=0;
+        int b=1;
+        while(N>1){
+        	int temp = b;
+        	b=a+b;
+        	a=temp;
+        	N--;
+        }
+        return b;
+    }
+}
