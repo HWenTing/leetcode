bc4a800f40f91a43eda205141c5bcbe0d4fe9809
HWenTing <982740445@qq.com>
2019-10-18 22:45:35 +0800

    简单题

---
+++ T_401_500/T402.java
@@ -0,0 +1,45 @@
+package T_401_500;
+
+public class T402 {
+
+//	给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+//
+//	num 的长度小于 10002 且 ≥ k。
+//	num 不会包含任何前导零。
+
+	public static void main(String[] args) {
+		System.out.println((int)'0');
+		System.out.println((int)'1');
+		System.out.println((int)'2');
+		System.out.println((int)'3');
+		System.out.println((int)'9');
+		
+	}
+//	每次去掉一位，最大程度减小数组
+//    public String removeKdigits(String num, int k) {
+//        
+//    }
+//    
+//    private String each(String num){
+//    	if(num.length()==1)
+//    		return "0";
+//
+//    	int 
+    	
+    	
+    	
+//    	if(num.charAt(1)=='0'){
+//    		int start=2;
+//    		while(start<num.length() && num.charAt(start)=='0')
+//    			start++;
+//    		if(start==num.length())
+//        		return "0";
+//    		else
+//    			return num.substring(start);
+//    	}else{
+//    		int max
+//    		for(int i)
+//    	}
+//    }
+    
+}
