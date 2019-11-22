7089a09df952be3d6059aff766e133f20e9e7712
HWenTing <982740445@qq.com>
2019-11-22 12:36:28 +0800

    双指针

---
+++ T_201_300/T209.java
@@ -0,0 +1,34 @@
+package T_201_300;
+
+public class T209 {
+
+	
+//	给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
+//	双指针 若当前和小于s，right++，反之left++
+    public int minSubArrayLen(int s, int[] nums) {
+    	if(nums.length==0) return 0;
+        int left=0;
+        int right=0;
+        int cursum=nums[0];
+        int ans=nums.length+1;
+        while(right<nums.length){
+        	if(cursum<s){
+        		right++;
+        		if(right<nums.length)
+        			cursum+=nums[right];
+        	}else{
+        		ans = Math.min(ans, right-left+1);
+        		cursum-=nums[left];
+        		left++;
+        		if(ans==1) return 1;
+        	}
+        }
+        return ans==nums.length+1?0:ans;
+        
+    }
+    
+    
+    
+    
+    
+}
