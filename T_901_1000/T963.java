0d7898b79d14886fddf6ca1f87c0a492b1a9fdcd
HWenTing <982740445@qq.com>
2019-11-29 10:24:01 +0800

    矩形

--- T_901_1000/T963.java
+++ T_901_1000/T963.java
@@ -1,6 +1,7 @@
 package T_901_1000;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 class Edge{
@@ -22,17 +23,41 @@ public class T963 {
 	
 //	遍历，三个角垂直，复杂度C(4,n) O(n^4)
 //	中点方法，因为对角线相等且中点相同是矩形的充要条件，所以复杂度可以降到O(n^2)
-//	用map存下<中点及长度,点1> 每次遇到相同的中点及长度就算一下
+//	用map存下<中点及长度,点> 每次遇到相同的中点及长度就算一下
 	
 	
     public double minAreaFreeRect(int[][] points) {
 
     	HashMap<String,List<Edge>> map = new HashMap<>();
         int len = points.length; 
+    	double ans=Double.MAX_VALUE;
+    	boolean has=false;
     	
-    	
-    	
-    	
+        for(int i=0;i<len-1;i++){
+        	for(int j=i+1;j<len;j++){
+        		int x1 = points[i][0];
+        		int y1 = points[i][1];
+        		int x2 = points[j][0];
+        		int y2 = points[j][1];
+        		double x = (x1+x2)/2.0;
+        		double y = (y1+y2)/2.0;
+        		double dis = Math.pow((x1-x2), 2)+ Math.pow((y1-y2), 2);
+        		if(!map.containsKey(x+" "+y+" "+dis)){//不存在相等的
+        			List<Edge> temp = new LinkedList<>();
+        			temp.add(new Edge(x1,y1,x2,y2));
+        			map.put(x+" "+y+" "+dis, temp);
+        		}else{
+        			for(Edge e:map.get(x+" "+y+" "+dis)){
+        				has=true;
+        				double are = Math.sqrt(Math.pow((x1-e.x1), 2)+ Math.pow((y1-e.y1), 2))*Math.sqrt(Math.pow((x1-e.x2), 2)+ Math.pow((y1-e.y2), 2));
+        				ans = Math.min(are, ans);
+        			}
+        			
+        			map.get(x+" "+y+" "+dis).add(new Edge(x1,y1,x2,y2));
+        		}
+        	}
+        }
+    	return has?ans:0;
     }
     
 }
