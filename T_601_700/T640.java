326731180a7859ed8271393ecb1e1e583079d47e
HWenTing <982740445@qq.com>
2020-01-29 00:09:08 +0800

    解方程

--- T_601_700/T640.java
+++ T_601_700/T640.java
@@ -2,16 +2,22 @@ package T_601_700;
 
 public class T640 {
 
-	
+//	求解一个给定的方程，将x以字符串"x=#value"的形式返回。该方程仅包含'+'，' - '操作，变量 x 和其对应系数。
+//	如果方程没有解，请返回“No solution”。
+//	如果方程有无限解，则返回“Infinite solutions”。
+//	如果方程中只有一个解，要保证返回值 x 是一个整数。
+
+//	字符串处理 
     public String solveEquation(String equation) {
     	
-    	equation = equation+"#";
-    	int leftnum = 0;
-    	int leftx = 0;
+    	equation = "#"+equation+"#";
+    	int leftnum = 0;//记录等号左边的数字
+    	int leftx = 0;//记录等号左边的系数
 
-    	int left = 1;
-    	int add = 1;
-    	int cur = 0;
+    	int left = 1;//标记左还是右
+    	int add = 1;//标记正还是负
+    	int cur = 0;//标记当前数字
+    	//left*add*cur综合考虑了符号和移相后的结果
     	
     	for(int i=0;i<equation.length();i++){
     		char c = equation.charAt(i);
@@ -25,7 +31,7 @@ public class T640 {
     			add=1;
     			break;
     		case 'x':
-    			if(cur==0 ) cur=1;
+    			if(cur==0 && equation.charAt(i-1)!='0') cur=1;//处理0x=0的情况
     			leftx+=left*add*cur;
     			cur=0;
     			break;
