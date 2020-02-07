ae02ad9505a27f8a30fa5a63603403f3cd33a6b5
HWenTing <982740445@qq.com>
2020-02-07 23:57:06 +0800

    扫雷

---
+++ T_501_600/T529.java
@@ -0,0 +1,17 @@
+package T_501_600;
+
+public class T529 {
+
+    public char[][] updateBoard(char[][] board, int[] click) {
+        if(board[click[0]][click[1]]=='M'){
+        	board[click[0]][click[1]] = 'X';
+        	return board;
+        }
+        	
+        
+        
+        
+        
+    }
+    
+}
